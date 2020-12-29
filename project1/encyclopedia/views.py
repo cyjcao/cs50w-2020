f01c16f6d192c7a1a1ee1d98c3e9e9323e781197
cyjcao <johncy.cao@gmail.com>
2020-12-29 17:07:15 -0500

    implemented search

--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
 
 from . import util
 import markdown2
@@ -12,6 +14,7 @@ def index(request):
 
 def entry(request, title):
     contents = util.get_entry(title)
+    # load page for corresponding entry if file exists, if not raise a 404 not found error
     if contents:
         return render(request, "encyclopedia/entry.html", {
             "title": title,
@@ -19,5 +22,26 @@ def entry(request, title):
         })
     else:
         raise Http404
+
+@require_http_methods(["POST"])
+def search(request):
+    query = request.POST['q']
+    contents = util.get_entry(query)
+    # if query matches name of an entry, redirect user to the entry's page
+    # if not, user is taken to search results page which contains list of entry names that has the query as a substring
+    if contents:
+        return HttpResponseRedirect(reverse('entry', args=[query]))
+    else:
+        all_entries = util.list_entries()
+        search_results = []
+        for entry in all_entries:
+            if query.lower() in entry.lower():
+                search_results.append(entry)
+        
+        return render(request, "encyclopedia/search.html", {
+            "title": "Search Results",
+            "results": search_results
+        })
+    
 
 
